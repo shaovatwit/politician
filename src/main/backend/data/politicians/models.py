cc44f6bbc8434e71bddbe536aa7610828db5ea59
Vianne Shao <viannewshao@gmail.com>
2023-03-06 18:22:14 +0100

    Django + PostgresSQL are setup.

---
+++ src/main/backend/data/politicians/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+class Politician(models.Model):
+        politician_id = models.AutoField(primary_key=True)
+        name = models.CharField(max_length=30)
+        gov_link = models.CharField(max_length=100, blank=True)
+        campaign_link = models.CharField(max_length=100, blank=True)
+        email = models.CharField(max_length=50, blank=True)
+        phone = models.CharField(max_length=15, blank=True)
+        address = models.CharField(max_length=100, blank=True)
+        party = models.CharField(max_length=25)
+        date_elected = models.DateField()
+        biography = models.CharField(max_length=100)
+        district = models.CharField(max_length=100)
+
+class City(models.Model):
+        city_id = models.AutoField(primary_key=True)
+        name = models.CharField(max_length=100)
+        state = models.CharField(max_length=25)
+        politician = models.ForeignKey(Politician, on_delete=models.CASCADE)
+
+class Office(models.Model):
+        office_id = models.AutoField(primary_key=True)
+        name = models.CharField(max_length=100)
+        role = models.CharField(max_length=100)
+        politician = models.ForeignKey(Politician, on_delete=models.CASCADE)
+    
+class Issue(models.Model):
+        issue_id = models.AutoField(primary_key=True)
+        name = models.CharField(max_length=100)
+        copy = models.CharField(max_length=100)
+        politician = models.ForeignKey(Politician, on_delete=models.CASCADE)
+
+class Committee(models.Model):
+        committee_id = models.AutoField(primary_key=True)
+        name = models.CharField(max_length=100)
+        chair = models.CharField(max_length=100)
+        groups_overseen = models.CharField(max_length=100)
+        committee_web = models.CharField(max_length=100, blank=True)
+        politician = models.ForeignKey(Politician, on_delete=models.CASCADE)
---
+++ src/main/backend/data/politicians/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+import requests
+from bs4 import BeautifulSoup
+
+from . import models
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Local Politician Data Aggregator.")
+
+#############################################################
+# Get politician's information from their website url input #
+#############################################################
+
+def get_politician_info(request, politician_id):
+    #Check if politician_id exists in the database... if so, get otherwise post
+    if request.method == "POST":
+        url = request.POST.get('url') #Get URL Input
+        req = request.get(url) #Make Request
+        web_s = req.text #Website of the request
+        soup = BeautifulSoup(web_s, "html.parser") #Parse
+        #Get name of the politician.
+        #Politician wishes for their citizens?
+
+        return render(request, "index.html", { #render to the index.html with the contents
+            #"name": name
+        })
+    return render(request, "index.html")
+
+###########################################################
+# Get city information and searches database for the      #
+# politician in that area. Then fetches the url from the  #
+# database to scrape the data                             #
+###########################################################
+
+def get_city(request, city_id):
+    if request.method == "POST":
+        url = request.POST.get('boston.gov site')
+        req = request.get(url)
+        web_s = req.text
+        soup = BeautifulSoup(web_s, "html.parser")
+
+        #Get number of councilors
+
+        return render(request, "whateverpage.html", {
+            #counsilors
+        })
+    return render(request, "wateverpage.html")
